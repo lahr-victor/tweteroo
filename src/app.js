633da37d6f6f26c94c9fdb2effa6f4ac41eb1b35
Victor Lahr <lahr.victor@gmail.com>
2023-04-10 12:44:59 -0300

    style: adequate code to Airbnb JS style guide

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
@@ -10,33 +10,31 @@ const PORT_NUMBER = 5000;
 const users = [];
 const tweets = [];
 
-app.post("/sign-up", (req, res) => {
-    const { username, avatar } = req.body;
-    users.push({ username, avatar });
-    res.status(201).send("OK");
+app.post('/sign-up', (req, res) => {
+  const { username, avatar } = req.body;
+  users.push({ username, avatar });
+  res.status(201).send('OK');
 });
 
-app.post("/tweets", (req, res) => {
-    const { username, tweet } = req.body;
-    const authorizedUser = users.some(user => user.username === username);
+app.post('/tweets', (req, res) => {
+  const { username, tweet } = req.body;
+  const authorizedUser = users.some((user) => user.username === username);
 
-    if (!authorizedUser) {
-        return res.status(401).send("UNAUTHORIZED");
-    }
+  if (!authorizedUser) {
+    return res.status(401).send('UNAUTHORIZED');
+  }
 
-    tweets.push({ id: tweets.length + 1, username, tweet });
-    res.status(201).send("OK");
+  tweets.push({ id: tweets.length + 1, username, tweet });
+  return res.status(201).send('OK');
 });
 
-app.get("/tweets", (req, res) => {
-    const updatedTweets = tweets.slice(-10).map((tweet) => {
-        const user = users.find(user => user.username === tweet.username);
-        return { username: tweet.username, avatar: user.avatar, tweet: tweet.tweet }
-    });
+app.get('/tweets', (req, res) => {
+  const updatedTweets = tweets.slice(-10).map((tweet) => {
+    const user = users.find((u) => u.username === tweet.username);
+    return { username: tweet.username, avatar: user.avatar, tweet: tweet.tweet };
+  });
 
-    res.status(200).send(updatedTweets);
+  res.status(200).send(updatedTweets);
 });
 
-app.listen(PORT_NUMBER, () =>
-    console.log(`Running server on port ${PORT_NUMBER}`)
-);
+app.listen(PORT_NUMBER, () => console.log(`Running server on port ${PORT_NUMBER}`));
