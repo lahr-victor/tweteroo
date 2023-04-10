dac219fa6872a00755789b625fe16622135854a4
Victor Lahr <lahr.victor@gmail.com>
2023-04-10 09:12:18 -0300

    feat: implement user sign-up POST request

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 const PORT_NUMBER = 5000;
+
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    users.push({ username, avatar });
+    res.status(201).send("OK");
+});
 
 app.listen(PORT_NUMBER, () =>
   console.log(`Running server on port ${PORT_NUMBER}`)
