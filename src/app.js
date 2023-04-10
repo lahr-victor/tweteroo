cd07d0ec6a33553deb67a6ef7120e7dfac9651a5
Victor Lahr <lahr.victor@gmail.com>
2023-04-10 10:11:30 -0300

    feat: implement user tweets POST request

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(cors());
 const PORT_NUMBER = 5000;
 
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
@@ -15,6 +16,18 @@ app.post("/sign-up", (req, res) => {
     res.status(201).send("OK");
 });
 
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    const authorizedUser = users.some(user => user.username === username);
+
+    if (!authorizedUser) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+
+    tweets.push({ id: tweets.length + 1, username, tweet });
+    res.status(201).send("OK");
+});
+
 app.listen(PORT_NUMBER, () =>
-  console.log(`Running server on port ${PORT_NUMBER}`)
+    console.log(`Running server on port ${PORT_NUMBER}`)
 );
